eaf577eff2370c1bce1fa82654b4e7d12a6d10c0
lenam20510 <lenam20510@gmail.com>
2017-03-28 13:16:08 +0700

    Added UsingIntent project

---
+++ UsingIntent/src/com/training/usingintent/UsingIntentActivity.java
@@ -0,0 +1,40 @@
+package com.training.usingintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class UsingIntentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.using_intent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void onClick(View v) {
+        startActivity(new Intent("com.training.secondactivity"));
+    }
+}
